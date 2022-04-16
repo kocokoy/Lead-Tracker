70ac890082bb4e0ec609996e8fff6208c1aca606
Vann Erycko <vanneryckoconstantino@gmail.com>
2022-04-16 16:11:35 +0800

    Update script.js

--- components/script.js
+++ components/script.js
@@ -1,16 +1,19 @@
 
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    console.log(myLeads)
-})
 
-    let listItems = "" 
-for (let i = 0; i < myLeads.length; i++){
-    listItems += "<li>" + myLeads[i] + "</li>"
-}
+    inputBtn.addEventListener("click", function() {
+        myLeads.push(inputEl.value)
+        renderLeads()
+    })
 
-ulEl.innerHTML = listItems
+    function renderLeads() {
+        let listItems = "" 
+        for (let i = 0; i < myLeads.length; i++){
+            listItems += "<li>" + myLeads[i] + "</li>"
+        }
+        ulEl.innerHTML = listItems
+
+    }
