f54960f77cdd561768eae0c2951ff3789d1f1235
Vann Erycko <vanneryckoconstantino@gmail.com>
2022-04-16 16:47:58 +0800

    Update script.js

--- components/script.js
+++ components/script.js
@@ -1,4 +1,3 @@
-
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
@@ -6,13 +5,18 @@ const ulEl = document.getElementById("ul-el")
 
     inputBtn.addEventListener("click", function() {
         myLeads.push(inputEl.value)
+        inputEl.value = ""
         renderLeads()
     })
 
     function renderLeads() {
         let listItems = "" 
         for (let i = 0; i < myLeads.length; i++){
-            listItems += "<li>" + myLeads[i] + "</li>"
+
+            listItems += `<li>
+            <a target='_blank' href='${myLeads[i]}'>${myLeads[i]}</a>
+            </li>`
+           
         }
         ulEl.innerHTML = listItems
 
