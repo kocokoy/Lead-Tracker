22fea4c9fae095c4fee3aeb6c37aae802fad3442
Vann Erycko <vanneryckoconstantino@gmail.com>
2022-04-20 23:13:50 +0800

    updated

--- src/script.js
+++ src/script.js
@@ -3,21 +3,26 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+
     inputBtn.addEventListener("click", function() {
         myLeads.push(inputEl.value)
         inputEl.value = ""
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
         renderLeads()
+        console.log(localStorage.getItem("myLeads"))
+
     })
 
     function renderLeads() {
         let listItems = "" 
         for (let i = 0; i < myLeads.length; i++){
 
-            listItems += `<li>
-            <a target='_blank' href='${myLeads[i]}'>${myLeads[i]}</a>
-            </li>`
-           
+            listItems += `<li> 
+                <a target='_blank' href='${myLeads[i]}'>${myLeads[i]}</a>
+                </li>`
         }
         ulEl.innerHTML = listItems
 
     }
+
+    
